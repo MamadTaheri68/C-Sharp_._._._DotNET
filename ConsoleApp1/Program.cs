6a9d5945d92601559b65d3d192be9c647ac701f0
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-27 23:20:12 +0430

    types

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            System.Data.SqlClient.SqlConnection cnn; // Creates null variable on stack and keeps the address;
+            cnn = new System.Data.SqlClient.SqlConnection(); // creates on heap
 
             var inta = 123;
             var boolb = true;
@@ -27,6 +29,20 @@ namespace ConsoleApp1
             Console.WriteLine(a.ToString());
             this.a = 123321456;
             Console.WriteLine(this.a.ToString());
+        }
+        public void valueTypes()
+        {
+            SByte a;
+            short b;
+            int c;
+            long d;
+            float e;
+            double f;
+            decimal g;
+            var temp = @"c:\\\\\\\\\\\\\";
+            Console.WriteLine(temp);
+            var str = "ali" + DateTime.Now + "data";
+            var str2 = $"ali {DateTime.Now.ToString()} reza";
         }
     }
 }
