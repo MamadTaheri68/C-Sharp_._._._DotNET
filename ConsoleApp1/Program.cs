291157de3387a9a6f197c74b0769945f13c9195c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-27 22:36:25 +0430

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var inta = 123;
+            var boolb = true;
+
+            Console.WriteLine(inta.GetType().ToString());
+            Console.WriteLine(boolb.GetType().ToString());
+            Console.ReadLine();
+        }
+    }
+}
