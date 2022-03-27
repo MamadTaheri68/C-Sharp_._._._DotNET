e9e78f7e274aa358969cc4ac6e937fa4783a21ba
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-28 00:42:50 +0430

    classess

---
+++ ConsoleApp1/FirstClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class FirstClass
+    {
+        private int _age;
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+        public int GetAge()
+        {
+            return _age;
+        }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public int MyProperty2 { get; }
+        public int MyProperty3 { get; private set; } = 12;
+
+        public string Fullname => $"{MyProperty2} {MyProperty3}";
+
+    }
+}
