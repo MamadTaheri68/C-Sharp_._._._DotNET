216ba1700208ccefb678a3be4107ee2a88dac36f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-28 21:17:27 +0430

    more work with functions

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private int _int1;
+        private int _int2;
+
+        private Class1():this(0,0)
+        {
+            _int1 = 5;
+        }
+        private Class1(int a, int b)
+        {
+            _int1 = a;
+            _int2 = b;
+        }
+
+        public int Add(int num01 = 3, int num02 = 2, int num03 = 3)
+        {
+            return num01 + num02;
+        }
+        public int Add(int num01, int num02) => num01 + num02;
+
+        public int sum(params int[] inputes)
+        {
+            int total = 0;
+            foreach (var item in inputes)
+            {
+                total += item;
+            }
+            return total;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibrary1;
+
 
 namespace ConsoleApp1
 {
@@ -10,6 +12,42 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Class1 c1 = new Class1();
+            //Console.WriteLine(c1.Add());
+            //Console.ReadLine();
+
+            //c1.sum();
+            //c1.sum(1);
+            //c1.sum(1,2,3,4,5,6,33,25,75);
+            //ClassTestRef testRef = new ClassTestRef();
+            //Console.WriteLine(testRef.A);
+            //ChangeA(testRef);
+            //Console.WriteLine(testRef.A);
+
+            int testref = 2 ;
+            int testOut;
+            ChangeByRef(ref testref);
+            ChangeByOut(out testOut);
+            
+            
+            Console.ReadLine();
+
+
+
+
+        }
+
+        private static void ChangeA(ClassTestRef testRef)
+        {
+            testRef.A = 123;
+        }
+        private static void ChangeByRef(ref int input)
+        {
+            input = 1;
+        }
+        private static void ChangeByOut(out int input)
+        {
+            input = 1;
         }
     }
 }
