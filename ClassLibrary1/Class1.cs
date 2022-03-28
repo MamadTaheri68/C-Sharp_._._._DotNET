caf98eeca8ef1971430f566e943b13e712113c12
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-28 21:20:34 +0430

    refresh

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,33 +8,6 @@ namespace ClassLibrary1
 {
     public class Class1
     {
-        private int _int1;
-        private int _int2;
-
-        private Class1():this(0,0)
-        {
-            _int1 = 5;
-        }
-        private Class1(int a, int b)
-        {
-            _int1 = a;
-            _int2 = b;
-        }
-
-        public int Add(int num01 = 3, int num02 = 2, int num03 = 3)
-        {
-            return num01 + num02;
-        }
-        public int Add(int num01, int num02) => num01 + num02;
-
-        public int sum(params int[] inputes)
-        {
-            int total = 0;
-            foreach (var item in inputes)
-            {
-                total += item;
-            }
-            return total;
-        }
+       
     }
 }
