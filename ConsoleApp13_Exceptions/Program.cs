d4c37d3393debf42c30c3b7c13aa3cfa7377f21c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-03-30 23:00:54 +0430

    Exception

---
+++ ConsoleApp13_Exceptions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13_Exceptions
+{
+    public class CustomException:Exception
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    CreateEx();
+            //}
+            ////catch (FileNotFoundException ex)
+            ////{
+            ////    Console.Write("An FileNotFoundException Exception happened");
+            ////}
+            ////catch(FieldAccessException ex)
+            ////{
+            ////    Console.Write("An FieldAccessException Exception happened");
+            ////}
+            //catch (Exception ex)
+            //{
+            //    //ex.Data.Add("FileName", "adfldkkdshf");
+
+            //    Console.Write("An Unknown Exception happened");
+            //}
+
+            string input = Console.ReadLine();
+
+            try
+            {
+                CreateEx();
+            }
+            catch (Exception) when(input == "Mamad")
+            {
+                Console.WriteLine("Mamad");
+            }
+            catch (Exception) when (input == "ali")
+            {
+                Console.WriteLine("ali");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception");
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void CreateEx()
+        {
+            try
+            {
+                CreateEx2();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("",ex);
+            }
+        }
+
+        private static void CreateEx2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
