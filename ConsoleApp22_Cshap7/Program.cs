5af320dd1eba45135032ab088d0276dffd27d730
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-04-02 00:50:12 +0430

    C# 7

---
+++ ConsoleApp22_Cshap7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22_Cshap7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var temp = new RefReturn();
+            temp.MyFunction();
+        }
+    }
+}
