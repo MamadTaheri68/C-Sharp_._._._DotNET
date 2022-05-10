e1f95b211fcec7b634b273c499d2aecb97d79087
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-10 14:05:23 +0430

    update Core project

--- 02-TDD in .NET/TicketingSolution/TicketingSolution.Core/Handler/TicketBookingRequestHandler.cs
+++ 02-TDD in .NET/TicketingSolution/TicketingSolution.Core/Handler/TicketBookingRequestHandler.cs
@@ -1,15 +1,31 @@
 ﻿using System;
+using TicketingSolution.Core.DataServices;
+using TicketingSolution.Core.Domain;
 
 namespace TicketingSolution.Core
 {
     public class TicketBookingRequestHandler
     {
-        public TicketBookingRequestHandler()
+        private readonly ITicketBookingService _ticketBookingService;
+
+        public TicketBookingRequestHandler(ITicketBookingService ticketBookingService)
         {
+            this._ticketBookingService = ticketBookingService;
         }
 
         public ServiceBookingResult BookService(TicketBookingRequest bookingRequest)
         {
+            if(bookingRequest is null)
+            {
+                throw new ArgumentNullException(nameof(bookingRequest));
+            }
+            _ticketBookingService.Save(new TicketBooking
+            {
+                Name = bookingRequest.Name,
+                Family = bookingRequest.Family,
+                Email = bookingRequest.Email,
+            });
+
             return new ServiceBookingResult
             {
                 Name = bookingRequest.Name,
