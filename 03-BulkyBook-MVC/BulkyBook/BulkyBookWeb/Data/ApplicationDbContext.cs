3b4b0a1c74a73270cb13a7eb1e7943cf10a32a67
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-12 00:34:19 +0430

    add ApplicationDbContext

---
+++ 03-BulkyBook-MVC/BulkyBook/BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        // جدول ها  
+        public DbSet<Category> Categories { get; set; }
+    }
+}
