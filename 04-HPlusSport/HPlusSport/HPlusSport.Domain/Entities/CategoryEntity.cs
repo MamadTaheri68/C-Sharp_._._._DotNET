adb2305048e4b4e636e4640be984eb8e0c67312e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-13 00:31:45 +0430

    move Entities to Domain Layer

---
+++ 04-HPlusSport/HPlusSport/HPlusSport.Domain/Entities/CategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPlusSport.Domain.Entities
+{
+    public class CategoryEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual List<ProductEntity> Products { get; set; }
+    }
+}
---
+++ 04-HPlusSport/HPlusSport/HPlusSport.Domain/Entities/ProductEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace HPlusSport.Domain.Entities
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual CategoryEntity Category { get; set; }
+    }
+}
