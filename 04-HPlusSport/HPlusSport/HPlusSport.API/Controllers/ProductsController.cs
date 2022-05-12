a9e020dcfab7d3f9955f0789436dcca1b0de91cc
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-12 23:53:47 +0430

    update ProductsController

--- 04-HPlusSport/HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
+++ 04-HPlusSport/HPlusSport/HPlusSport.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HPlusSport.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HPlusSport.API.Controllers
@@ -7,5 +8,16 @@ namespace HPlusSport.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToArray();
+        }
     }
 }
