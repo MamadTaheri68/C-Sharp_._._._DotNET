84a7e5c77c38e425d9b43261e6d15152c983e127
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-13 22:31:47 +0430

    implement GetById and AddHero and UpdateHero

--- 05-SuperHeroAPI/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
+++ 05-SuperHeroAPI/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -16,13 +16,53 @@ namespace SuperHeroAPI.Controllers
                     FirstName = "Peter",
                     LastName = "Parker",
                     Place = "New York City"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
                 }
             };
 
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
-            
+            return Ok(heroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetById(int id)
+        {
+            var hero = heroes.Find(q => q.Id == id);
+            if (hero == null)
+            {
+                return NotFound("Heo Not Found");
+            }
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero updatedHero)
+        {
+            var hero = heroes.Find(q => q.Id == updatedHero.Id);
+            if(hero == null)
+            {
+                return BadRequest("Hero Not found");
+            }
+            hero.Name = updatedHero.Name;
+            hero.FirstName = updatedHero.FirstName;
+            hero.LastName = updatedHero.LastName;
+            hero.Place = updatedHero.Place;
 
             return Ok(heroes);
         }
