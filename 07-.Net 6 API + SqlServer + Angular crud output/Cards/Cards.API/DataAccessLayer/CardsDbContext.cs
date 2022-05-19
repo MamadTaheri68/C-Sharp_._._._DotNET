044d53f1bb81d7705da57f5b1f68fda2a6472b2f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-19 20:22:45 +0430

    add & inject context

---
+++ 07-.Net 6 API + SqlServer + Angular crud output/Cards/Cards.API/DataAccessLayer/CardsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cards.API.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cards.API.DataAccessLayer
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
--- 07-.Net 6 API + SqlServer + Angular crud output/Cards/Cards.API/Program.cs
+++ 07-.Net 6 API + SqlServer + Angular crud output/Cards/Cards.API/Program.cs
@@ -1,3 +1,6 @@
+using Cards.API.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject DbContext to Services
+builder.Services.AddDbContext<CardsDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
