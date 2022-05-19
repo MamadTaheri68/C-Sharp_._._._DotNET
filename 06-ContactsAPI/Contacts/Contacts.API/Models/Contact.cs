94a2c2ed1887965a49e266c159a1b89561b498d0
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-19 15:44:07 +0430

    add Contact Model

---
+++ 06-ContactsAPI/Contacts/Contacts.API/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Contacts.API.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public string String { get; set; }
+
+    }
+}
