37ca62c0d6e32e848866237eaa978780da617809
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-19 15:50:47 +0430

    create and inject our Context

---
+++ 06-ContactsAPI/Contacts/Contacts.API/DataAccessLayer/ContactAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Contacts.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.API.DataAccessLayer
+{
+    public class ContactAPIDbContext : DbContext
+    {
+        public ContactAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
--- 06-ContactsAPI/Contacts/Contacts.API/Models/Contact.cs
+++ 06-ContactsAPI/Contacts/Contacts.API/Models/Contact.cs
@@ -6,7 +6,7 @@
         public string FullName { get; set; }
         public string Email { get; set; }
         public long Phone { get; set; }
-        public string String { get; set; }
+        public string Address { get; set; }
 
     }
 }
