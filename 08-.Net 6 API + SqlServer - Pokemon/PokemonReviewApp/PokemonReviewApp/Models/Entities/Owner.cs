583c7d893c8a1d202940120f6f4406ee78eb8305
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-20 01:43:16 +0430

    add models

---
+++ 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Models/Entities/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+    }
+}
---
+++ 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Models/Entities/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp.Models.Entities
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
