9b795663ad6433c65a3beafcb887225a7fd7e234
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-20 13:35:25 +0430

    handle DB

---
+++ 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Data/PokemonDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Models.Entities;
+
+namespace PokemonReviewApp.Data
+{
+    public class PokemonDbContext : DbContext
+    {
+        public PokemonDbContext(DbContextOptions<PokemonDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(q => new { q.PokemonId, q.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(q => q.Pokemon)
+                .WithMany(p => p.PokemonCategories)
+                .HasForeignKey(s => s.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(q => q.Category)
+                .WithMany(p => p.PokemonCategories)
+                .HasForeignKey(s => s.CategoryId);
+
+            modelBuilder.Entity<PokemonOwner>()
+              .HasKey(q => new { q.PokemonId, q.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(q => q.Pokemon)
+                .WithMany(p => p.PokemonOwners)
+                .HasForeignKey(s => s.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(q => q.Owner)
+                .WithMany(p => p.PokemonOwners)
+                .HasForeignKey(s => s.OwnerId);
+        }
+    }
+}
