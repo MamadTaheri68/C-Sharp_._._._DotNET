837ebcfccef56e6f7b28712702106f6fefa92194
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-20 12:28:50 +0430

    ad  Many to Many relationship

---
+++ 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Models/Entities/PokemonOwner.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models.Entities
+{
+    // Join Table
+    public class PokemonOwner
+    {
+        public int PokemonId { get; set; }
+        public int OwnerId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
