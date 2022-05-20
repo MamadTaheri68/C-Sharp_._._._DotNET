829666a6942c4af4483c6b52ac4f4509ad490a07
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-20 18:59:13 +0430

    extedn Pokemon functionality

---
+++ 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonReviewApp.Models.DTOs;
+using PokemonReviewApp.Models.Entities;
+
+namespace PokemonReviewApp.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
--- 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Repository/PokemonRepository.cs
+++ 08-.Net 6 API + SqlServer - Pokemon/PokemonReviewApp/PokemonReviewApp/Repository/PokemonRepository.cs
@@ -13,9 +13,33 @@ namespace PokemonReviewApp.Repository
             _context = pokemonDbContext;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(q => q.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(q => q.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokemonId)
+        {
+            var review = _context.Reviews.Where(q => q.Pokemon.Id == pokemonId);
+            if(review.Count() <= 0)
+                return 0;
+
+            return ((decimal)review.Sum(q => q.Rating) / review.Count());
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(q => q.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(q => q.Id == pokeId);
         }
     }
 }
