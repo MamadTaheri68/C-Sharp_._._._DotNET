d290dd2957b3524afa764ea50120ed0462bd79df
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-27 21:06:20 +0430

    Add Mapping using FluentAPI

---
+++ 12 - EFCoreProject/EFCoreProject/Infrastructure/EFCoreProject.Infrastructure.EFCore/Mapping/ProductCategoryMapping.cs
@@ -0,0 +1,21 @@
+﻿using EFCoreProject.Domain.ProductCategoryAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCoreProject.Infrastructure.EFCore.Mapping
+{
+    public class ProductCategoryMapping : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> builder)
+        {
+            builder.ToTable("ProductCategories");
+            builder.HasKey(x => x.Id);
+            builder.Property(q => q.Name).HasMaxLength(255).IsRequired();
+
+            builder
+                .HasMany(q => q.Products)
+                .WithOne(q => q.Category)
+                .HasForeignKey(q => q.CategoryId);
+        }
+    }
+}
---
+++ 12 - EFCoreProject/EFCoreProject/Infrastructure/EFCoreProject.Infrastructure.EFCore/Mapping/ProductMapping.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCoreProject.Domain.ProductAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFCoreProject.Infrastructure.EFCore.Mapping
+{
+    public class ProductMapping : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(255).IsRequired();
+
+            builder
+                .HasOne(q => q.Category)
+                .WithMany(q => q.Products)
+                .HasForeignKey(q => q.CategoryId);
+        }
+    }
+}
