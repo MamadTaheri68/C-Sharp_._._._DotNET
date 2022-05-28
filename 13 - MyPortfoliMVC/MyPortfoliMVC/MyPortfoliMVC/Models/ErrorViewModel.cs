d5f55ad627bce5a7b742b444262260cd7271628b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-28 21:41:48 +0430

    add project MyPortfoliMVC

---
+++ 13 - MyPortfoliMVC/MyPortfoliMVC/MyPortfoliMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPortfoliMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
