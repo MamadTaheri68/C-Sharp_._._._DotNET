a8fd0689243fb207477e1960a58d6f5eaea1aedc
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-29 15:20:09 +0430

    worjing with MVS

--- 13 - MyPortfolioMVC/MyPortfolioMVC/MyPortfolioMVC/Controllers/HomeController.cs
+++ 13 - MyPortfolioMVC/MyPortfolioMVC/MyPortfolioMVC/Controllers/HomeController.cs
@@ -6,20 +6,47 @@ namespace MyPortfolioMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Portfoli> _portfolios = new List<Portfoli>
+        {
+            new Portfoli
+            {
+                Id = 1, Title = "نمونه یک", Image = "01.jpg", Description = "این اولین پروژه است"
+            },
+            new Portfoli
+            {
+                Id = 2, Title = "نمونه دو", Image = "02.jpg", Description = "این دومین پروژه است"
+            },
+            new Portfoli
+            {
+                Id = 3, Title = "نمونه سه", Image = "03.jpg", Description = "این سومین پروژه است"
+            },
+            new Portfoli
+            {
+                Id = 4, Title = "نمونه چهار", Image = "04.jpg", Description = "این چهارمین پروژه است"
+            }
+        };
+
         public IActionResult Index()
         {
-            return View();
+            ViewData["headingTitle"] = "به سایت شخصی من خوش آمدید";
+            ViewData["headingText"] = "این سایت جهت نمونه کار آماده گردیده است";
+            ViewBag.aaa = "لورم ایپسوم";
+            return View(_portfolios);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
-            return View();
+            var contact = new Contact()
+            {
+                Email = "mamad.taheri@gmail.com",
+                Mobile = "09123456789",
+                Github = "https://github.com/MamadTaheri68"
+            };
+            return View(contact);
         }
+        
 
-        public IActionResult Shop()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
