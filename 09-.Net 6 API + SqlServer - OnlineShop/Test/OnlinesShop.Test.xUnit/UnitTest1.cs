b7e702c90b67911433515b8f19b06472a2a0befa
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-05-31 00:07:19 +0430

    add Test

--- 09-.Net 6 API + SqlServer - OnlineShop/Test/OnlinesShop.Test.xUnit/UnitTest1.cs
+++ 09-.Net 6 API + SqlServer - OnlineShop/Test/OnlinesShop.Test.xUnit/UnitTest1.cs
@@ -5,13 +5,23 @@ namespace OnlinesShop.Test.xUnit
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_Return_True()
         {
             var testString = "mamdad";
 
             bool result = (testString == "mamdad");
 
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Should_Return_False()
+        {
+            var testString = "mamdad";
+
+            bool result = (testString == "mmm");
+
+            Assert.False(result);
         }
 
     }
