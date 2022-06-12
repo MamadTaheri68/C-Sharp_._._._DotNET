167a1d191caec83fefd5f8861135d1b7280e0b3f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-12 23:34:27 +0430

    implement new project with test

---
+++ 003-Usefull-Projects/Bad_Tax_Calculator/Bad_TaxCalculator.Test/IndividualTaxCalculatorTest.cs
@@ -0,0 +1,29 @@
+using Bad_TaxCalculator.Entity;
+using System;
+using Xunit;
+
+namespace Bad_TaxCalculator.Test
+{
+    public class IndividualTaxCalculatorTest
+    {
+        [Fact]
+        public void None_TaxResident_SHOULD_NOT_BE_Calculated()
+        {
+            // Arrange
+            TaxPayer taxPayer = new TaxPayer
+            {
+                TaxCitizen = false
+            };
+            IndividualTaxCalculator individualTaxCalculator = new IndividualTaxCalculator();
+
+            // Act
+            string expectedErrorMessage = "No TAX Calculation for NON-TAX REsidents";
+
+            var ExceptionResult = Assert.Throws<InvalidOperationException>(() => 
+            individualTaxCalculator.CalculateTaxPercentage(taxPayer));
+
+            // Assert
+            Assert.Equal(expectedErrorMessage, ExceptionResult.Message);
+        }
+    }
+}
---
+++ 003-Usefull-Projects/Bad_Tax_Calculator/Bad_TaxCalculator/Entity/TaxPayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bad_TaxCalculator.Entity
+{
+    public class TaxPayer
+    {
+        public bool TaxCitizen { get; set; }
+    }
+}
