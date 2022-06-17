851bd830f7e48507323b1d0d46011ca6d420d8e7
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-18 00:58:52 +0430

    add IProductService

---
+++ 003-Usefull-Projects/OnlineShop-Deram/Src/03.Application/OnlineShop-Deram.Application/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using OnlineShop_Deram.Core.Entities;
+using OnlineShop_Deram.Infrastructure.Dto;
+
+namespace OnlineShop_Deram.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<List<ProductDto>> GetAll();
+        Task<ProductDto> Get(int id);
+        Task<ProductDto> Add(ProductDto model);
+    }
+}
