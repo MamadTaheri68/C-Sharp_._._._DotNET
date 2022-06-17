0c042dd9751a090664e6f9b7a2f5005fb4761409
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-18 00:10:39 +0430

    add ProductDto

---
+++ 003-Usefull-Projects/OnlineShop-Deram/Src/02.Infrastructure/OnlineShop-Deram.Infrastructure/Dto/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop_Deram.Infrastructure.Dto
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public long Price { get; set; }
+        public string PriceWithComma { get; set; }
+    }
+}
